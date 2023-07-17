e2da1caf3bf73306c952ba058c781f199d1fee2a
Karl760 <113386381+Kai7609@users.noreply.github.com>
2023-07-17 19:41:55 -0400

    Update fizzBuzz.ts

--- src/fizzBuzz.ts
+++ src/fizzBuzz.ts
@@ -1,19 +1,26 @@
 export default function fizzBuzz (n : number) : string[] {
     let result : string[] = [];
+    let three : boolean = false;
+    let five : boolean = false;
 
     for(let i = 1; i < n + 1; i++){
-        if((i % 3 == 0) && !(i % 5 == 0)){
+        three = i % 3 == 0;
+        five = i % 5 == 0;
+
+        if(three && !five){
             result.push("Fizz");
         }
-        else if(!(i % 3 == 0) && (i % 5 == 0)){
+        else if(!three && five){
             result.push("Buzz");
         }
-        else if((i % 3 == 0) && (i % 5 == 0)){
+        else if(three && five){
             result.push("FizzBuzz");
         }
         else{
             result.push(i.toString());
         }
+        three = false;
+        five = false;
     }
     return result;
 };
